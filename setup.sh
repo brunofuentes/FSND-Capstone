8bdc90eb4fd64a74371ca3017227f4b7b4901210
Bruno <brunofuentess@gmail.com>
2021-12-10 00:06:52 +0100

    add database_url

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-export DATABASE_URL = ""
+export DATABASE_URL = "postgres://hmehfvolvacjgm:b57df84e89493139cf0bfa9dc81822c29d0329f37ecd60d725ad84b9f0ed5681@ec2-3-89-214-80.compute-1.amazonaws.com:5432/ddkeo7cg646hoj"
